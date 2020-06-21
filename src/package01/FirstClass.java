175c1e2d780f06b41dd603d6553f95ce79a2478a
hasanbasri <hasanbasriuysal3@gmail.com>
2020-06-21 21:51:10 +0300

    İlk commit from local repository

--- src/package01/FirstClass.java
+++ src/package01/FirstClass.java
@@ -3,7 +3,8 @@ package package01;
 public class FirstClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Ýlk commit from local repository");
+		
 
 	}
 
